3e140c71aa8d2efd1521962d2496189c21b8d5f5
idalov <dep24cpp@yandex.ru>
2017-02-25 12:54:26 +0300

    #операторы сдвига

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
 int main() {
-    int a = 5;
-     //a=a+1
+    int8_t a = 5;     //00000101
+    a=a>>1;
+    printf("%d\n",a); //00000010
+    a=a<<1;
+    printf("%d\n",a); //00000100
+
+    int8_t b = -128;
+    b = b<<1;
+    printf("%d\n",b);
+    int8_t c = -128;    //при сдвиге вправо знак сохраняется
+    c = c>>4;
+    printf("%d\n",c);
+
+   /*  //a=a+1
     printf("%d\n",++a);
-    printf("%d\n",a++);
+    printf("%d\n",a++);*/
 
     return 0;
 }
